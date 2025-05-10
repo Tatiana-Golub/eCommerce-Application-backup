b6dfffc89d6a43d5ed90f17b91ee6112f07f6c36
Tatiana Golub <tatiana.k1989@gmail.com>
2025-05-10 19:28:31 +0200

    refactor: rework login page

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import BaseComponent from '@common-components/base-component';
 import {
   createButton,
@@ -16,47 +17,61 @@ class LoginComponent extends BaseComponent<HTMLDivElement> {
   private readonly form: BaseComponent<HTMLFormElement>;
 
   private readonly emailInputDiv: BaseComponent<HTMLDivElement>;
-  private emailInput: BaseComponent<HTMLInputElement> | undefined;
-  private emailTooltip: BaseComponent<HTMLDivElement> | undefined;
-  private emailFormat: BaseComponent<HTMLSpanElement> | undefined;
-  private emailWhitespace: BaseComponent<HTMLSpanElement> | undefined;
-  private emailAt: BaseComponent<HTMLSpanElement> | undefined;
-  private emailDomain: BaseComponent<HTMLSpanElement> | undefined;
+  private readonly emailInput: BaseComponent<HTMLInputElement>;
+  private readonly emailTooltip: BaseComponent<HTMLDivElement>;
+  private readonly emailFormat: BaseComponent<HTMLSpanElement>;
+  private readonly emailWhitespace: BaseComponent<HTMLSpanElement>;
+  private readonly emailAt: BaseComponent<HTMLSpanElement>;
+  private readonly emailDomain: BaseComponent<HTMLSpanElement>;
 
   private readonly passwordInputDiv: BaseComponent<HTMLDivElement>;
   private isPasswordVisible: boolean = false;
-  private passwordInput: BaseComponent<HTMLInputElement> | undefined;
-  private passwordControl: BaseComponent<HTMLDivElement> | undefined;
-  private passwordTooltip: BaseComponent<HTMLDivElement> | undefined;
-  private passwordLength: BaseComponent<HTMLSpanElement> | undefined;
-  private passwordUppercase: BaseComponent<HTMLSpanElement> | undefined;
-  private passwordLowercase: BaseComponent<HTMLSpanElement> | undefined;
-  private passwordDigit: BaseComponent<HTMLSpanElement> | undefined;
-  private passwordSpecial: BaseComponent<HTMLSpanElement> | undefined;
-  private passwordWhitespace: BaseComponent<HTMLSpanElement> | undefined;
+  private readonly passwordInput: BaseComponent<HTMLInputElement>;
+  private readonly passwordControl: BaseComponent<HTMLDivElement>;
+  private readonly passwordTooltip: BaseComponent<HTMLDivElement>;
+  private readonly passwordLength: BaseComponent<HTMLSpanElement>;
+  private readonly passwordUppercase: BaseComponent<HTMLSpanElement>;
+  private readonly passwordLowercase: BaseComponent<HTMLSpanElement>;
+  private readonly passwordDigit: BaseComponent<HTMLSpanElement>;
+  private readonly passwordSpecial: BaseComponent<HTMLSpanElement>;
+  private readonly passwordWhitespace: BaseComponent<HTMLSpanElement>;
 
   private readonly submitButton: BaseComponent<HTMLButtonElement>;
 
-  constructor(id: string = 'login-component', className: string = 'container') {
+  constructor(id: string = 'login-component', className: string = 'login-component') {
     super(Tags.DIV, id, className);
 
     this.form = createForm(undefined, 'login-form');
 
     this.emailInputDiv = this.createEmailInputDiv();
-    this.emailInputDiv.appendTo(this.form.getElement());
+    this.emailInput = this.createEmailInput();
+    this.emailTooltip = this.createEmailTooltip();
+    this.emailFormat = this.createEmailFormat();
+    this.emailWhitespace = this.createEmailWhitespace();
+    this.emailAt = this.createEmailAt();
+    this.emailDomain = this.createEmailDomain();
 
     this.passwordInputDiv = this.createPasswordInputDiv();
-    this.passwordInputDiv.appendTo(this.form.getElement());
+    this.passwordInput = this.createPasswordInput();
+    this.passwordControl = this.createPasswordControl();
+    this.passwordTooltip = this.createPasswordTooltip();
+    this.passwordLength = this.createPasswordLength();
+    this.passwordUppercase = this.createPasswordUppercase();
+    this.passwordLowercase = this.createPasswordLowercase();
+    this.passwordDigit = this.createPasswordDigit();
+    this.passwordSpecial = this.createPasswordSpecial();
+    this.passwordWhitespace = this.createPasswordWhitespace();
 
-    this.submitButton = createButton(undefined, 'button');
-    this.submitButton.setText('Sign In');
-    this.submitButton.appendTo(this.form.getElement());
+    this.submitButton = this.createSubmitButton();
 
     this.init();
   }
 
   protected renderComponent(): void {
     this.renderForm();
+    this.renderEmailInputDiv();
+    this.renderPasswordInputDiv();
+    this.renderSubmitButton();
   }
 
   protected addEventListeners(): void {
@@ -78,16 +93,13 @@ class LoginComponent extends BaseComponent<HTMLDivElement> {
     const hasAt = emailRules.hasAt.test(email);
     const hasDomain = emailRules.hasDomain.test(email);
 
-    if (this.emailFormat)
-      this.emailFormat.getElement().style.display = isValidFormat ? 'none' : 'block';
-    if (this.emailWhitespace)
-      this.emailWhitespace.getElement().style.display = isValidWhitespace ? 'none' : 'block';
-    if (this.emailAt) this.emailAt.getElement().style.display = hasAt ? 'none' : 'block';
-    if (this.emailDomain)
-      this.emailDomain.getElement().style.display = hasDomain ? 'none' : 'block';
+    this.emailFormat.getElement().style.display = isValidFormat ? 'none' : 'block';
+    this.emailWhitespace.getElement().style.display = isValidWhitespace ? 'none' : 'block';
+    this.emailAt.getElement().style.display = hasAt ? 'none' : 'block';
+    this.emailDomain.getElement().style.display = hasDomain ? 'none' : 'block';
 
     const isValid = isValidFormat && isValidWhitespace && hasAt && hasDomain;
-    if (this.emailTooltip) this.emailTooltip.getElement().hidden = isValid || email === '';
+    this.emailTooltip.getElement().hidden = isValid || email === '';
   }
 
   private validatePassword(): void {
@@ -100,18 +112,12 @@ class LoginComponent extends BaseComponent<HTMLDivElement> {
     const isValidSpecialChar = passwordRules.specialChar.test(password);
     const isValidWhitespace = passwordRules.noWhitespace.test(password);
 
-    if (this.passwordLength)
-      this.passwordLength.getElement().style.display = isValidLength ? 'none' : 'block';
-    if (this.passwordUppercase)
-      this.passwordUppercase.getElement().style.display = isValidUpperCase ? 'none' : 'block';
-    if (this.passwordLowercase)
-      this.passwordLowercase.getElement().style.display = isValidLowerCase ? 'none' : 'block';
-    if (this.passwordDigit)
-      this.passwordDigit.getElement().style.display = isValidDigit ? 'none' : 'block';
-    if (this.passwordSpecial)
-      this.passwordSpecial.getElement().style.display = isValidSpecialChar ? 'none' : 'block';
-    if (this.passwordWhitespace)
-      this.passwordWhitespace.getElement().style.display = isValidWhitespace ? 'none' : 'block';
+    this.passwordLength.getElement().style.display = isValidLength ? 'none' : 'block';
+    this.passwordUppercase.getElement().style.display = isValidUpperCase ? 'none' : 'block';
+    this.passwordLowercase.getElement().style.display = isValidLowerCase ? 'none' : 'block';
+    this.passwordDigit.getElement().style.display = isValidDigit ? 'none' : 'block';
+    this.passwordSpecial.getElement().style.display = isValidSpecialChar ? 'none' : 'block';
+    this.passwordWhitespace.getElement().style.display = isValidWhitespace ? 'none' : 'block';
 
     const isValid =
       isValidLength &&
@@ -120,16 +126,15 @@ class LoginComponent extends BaseComponent<HTMLDivElement> {
       isValidDigit &&
       isValidWhitespace &&
       isValidSpecialChar;
-    if (this.passwordTooltip) this.passwordTooltip.getElement().hidden = isValid || password === '';
+    this.passwordTooltip.getElement().hidden = isValid || password === '';
   }
 
   private addEventListenerPasswordControl(): void {
     this.passwordControl?.addEventListener('click', () => {
       this.isPasswordVisible = !this.isPasswordVisible;
-      if (this.passwordInput)
-        this.passwordInput.getElement().type = this.isPasswordVisible
-          ? InputType.TEXT
-          : InputType.PASSWORD;
+      this.passwordInput.getElement().type = this.isPasswordVisible
+        ? InputType.TEXT
+        : InputType.PASSWORD;
       this.passwordControl?.removeChildren();
       this.passwordControl
         ?.getElement()
@@ -141,82 +146,157 @@ class LoginComponent extends BaseComponent<HTMLDivElement> {
     this.form.appendTo(this.getElement());
   }
 
-  private createEmailInputDiv(): BaseComponent<HTMLDivElement> {
-    const emailInputDiv = createDiv(undefined, 'email-input');
+  private renderSubmitButton(): void {
+    this.submitButton.appendTo(this.form.getElement());
+  }
 
-    this.emailInput = createInput(undefined, 'email');
-    const emailInputElement = this.emailInput.getElement();
+  private renderPasswordInputDiv(): void {
+    this.passwordInputDiv.appendTo(this.form.getElement());
+    this.passwordInput.appendTo(this.passwordInputDiv.getElement());
+    this.passwordControl.appendTo(this.passwordInputDiv.getElement());
+    this.passwordTooltip.appendTo(this.passwordInputDiv.getElement());
+    this.passwordLength.appendTo(this.passwordTooltip.getElement());
+    this.passwordUppercase.appendTo(this.passwordTooltip.getElement());
+    this.passwordLowercase.appendTo(this.passwordTooltip.getElement());
+    this.passwordDigit.appendTo(this.passwordTooltip.getElement());
+    this.passwordSpecial.appendTo(this.passwordTooltip.getElement());
+    this.passwordWhitespace.appendTo(this.passwordTooltip.getElement());
+  }
+
+  private renderEmailInputDiv(): void {
+    this.emailInputDiv.appendTo(this.form.getElement());
+    this.emailInput.appendTo(this.emailInputDiv.getElement());
+    this.emailTooltip.appendTo(this.emailInputDiv.getElement());
+    this.emailFormat.appendTo(this.emailTooltip.getElement());
+    this.emailWhitespace.appendTo(this.emailTooltip.getElement());
+    this.emailAt.appendTo(this.emailTooltip.getElement());
+    this.emailDomain.appendTo(this.emailTooltip.getElement());
+  }
+
+  private createEmailInputDiv(): BaseComponent<HTMLDivElement> {
+    return createDiv(undefined, 'email-input');
+  }
+
+  private createEmailDomain(): BaseComponent<HTMLSpanElement> {
+    const emailDomain = createSpan(undefined, 'error-message');
+    emailDomain.setText('Email must include a domain (e.g., example.com)');
+
+    return emailDomain;
+  }
+
+  private createEmailAt(): BaseComponent<HTMLSpanElement> {
+    const emailAt = createSpan(undefined, 'error-message');
+    emailAt.setText('Email must contain an "@" symbol');
+
+    return emailAt;
+  }
+
+  private createEmailWhitespace(): BaseComponent<HTMLSpanElement> {
+    const emailWhitespace = createSpan(undefined, 'error-message');
+    emailWhitespace.setText('Email must not contain leading or trailing whitespace');
+
+    return emailWhitespace;
+  }
+
+  private createEmailFormat(): BaseComponent<HTMLSpanElement> {
+    const emailFormat = createSpan(undefined, 'error-message');
+    emailFormat.setText('Email must be properly formatted (e.g., user@example.com)');
+
+    return emailFormat;
+  }
+
+  private createEmailTooltip(): BaseComponent<HTMLDivElement> {
+    const emailTooltip = createDiv(undefined, 'tooltip');
+    emailTooltip.getElement().hidden = true;
+
+    return emailTooltip;
+  }
+
+  private createEmailInput(): BaseComponent<HTMLInputElement> {
+    const emailInput = createInput(undefined, 'email');
+    const emailInputElement = emailInput.getElement();
     emailInputElement.placeholder = 'Enter your e-mail';
     emailInputElement.type = InputType.TEXT;
-    this.emailInput.appendTo(emailInputDiv.getElement());
 
-    this.emailTooltip = createDiv(undefined, 'tooltip');
-    this.emailTooltip.getElement().hidden = true;
-    this.emailTooltip.appendTo(emailInputDiv.getElement());
-
-    this.emailFormat = createSpan(undefined, 'error-message');
-    this.emailFormat.setText('Email must be properly formatted (e.g., user@example.com)');
-    this.emailFormat.appendTo(this.emailTooltip.getElement());
-
-    this.emailWhitespace = createSpan(undefined, 'error-message');
-    this.emailWhitespace.setText('Email must not contain leading or trailing whitespace');
-    this.emailWhitespace.appendTo(this.emailTooltip.getElement());
-
-    this.emailAt = createSpan(undefined, 'error-message');
-    this.emailAt.setText('Email must contain an "@" symbol');
-    this.emailAt.appendTo(this.emailTooltip.getElement());
-
-    this.emailDomain = createSpan(undefined, 'error-message');
-    this.emailDomain.setText('Email must include a domain (e.g., example.com)');
-    this.emailDomain.appendTo(this.emailTooltip.getElement());
-
-    return emailInputDiv;
+    return emailInput;
   }
 
   private createPasswordInputDiv(): BaseComponent<HTMLDivElement> {
-    const passwordInputDiv = createDiv(undefined, 'password-input');
+    return createDiv(undefined, 'password-input');
+  }
 
-    this.passwordInput = createInput(undefined, 'password');
-    const passwordInputElement = this.passwordInput.getElement();
-    passwordInputElement.placeholder = 'Enter your password';
-    passwordInputElement.type = InputType.PASSWORD;
-    this.passwordInput.appendTo(passwordInputDiv.getElement());
+  private createPasswordWhitespace(): BaseComponent<HTMLSpanElement> {
+    const passwordWhitespace = createSpan(undefined, 'error-message');
+    passwordWhitespace.setText('Password must not contain leading or trailing whitespace');
 
-    this.passwordControl = createDiv(undefined, 'password-control');
-    this.passwordControl.getElement().insertAdjacentHTML('afterbegin', eyeClose);
-    this.passwordControl.appendTo(passwordInputDiv.getElement());
+    return passwordWhitespace;
+  }
 
-    this.passwordTooltip = createDiv(undefined, 'tooltip');
-    this.passwordTooltip.getElement().hidden = true;
-    this.passwordTooltip.appendTo(passwordInputDiv.getElement());
-
-    this.passwordLength = createSpan(undefined, 'error-message');
-    this.passwordLength.setText('Password must be at least 8 characters long');
-    this.passwordLength.appendTo(this.passwordTooltip.getElement());
-
-    this.passwordUppercase = createSpan(undefined, 'error-message');
-    this.passwordUppercase.setText('Password must contain at least one uppercase letter (A-Z)');
-    this.passwordUppercase.appendTo(this.passwordTooltip.getElement());
-
-    this.passwordLowercase = createSpan(undefined, 'error-message');
-    this.passwordLowercase.setText('Password must contain at least one lowercase letter (a-z)');
-    this.passwordLowercase.appendTo(this.passwordTooltip.getElement());
-
-    this.passwordDigit = createSpan(undefined, 'error-message');
-    this.passwordDigit.setText('Password must contain at least one digit (0-9)');
-    this.passwordDigit.appendTo(this.passwordTooltip.getElement());
-
-    this.passwordSpecial = createSpan(undefined, 'error-message');
-    this.passwordSpecial.setText(
+  private createPasswordSpecial(): BaseComponent<HTMLSpanElement> {
+    const passwordSpecial = createSpan(undefined, 'error-message');
+    passwordSpecial.setText(
       'Password must contain at least one special character (e.g., !@#$%^&*)',
     );
-    this.passwordSpecial.appendTo(this.passwordTooltip.getElement());
 
-    this.passwordWhitespace = createSpan(undefined, 'error-message');
-    this.passwordWhitespace.setText('Password must not contain leading or trailing whitespace');
-    this.passwordWhitespace.appendTo(this.passwordTooltip.getElement());
+    return passwordSpecial;
+  }
 
-    return passwordInputDiv;
+  private createPasswordDigit(): BaseComponent<HTMLSpanElement> {
+    const passwordDigit = createSpan(undefined, 'error-message');
+    passwordDigit.setText('Password must contain at least one digit (0-9)');
+
+    return passwordDigit;
+  }
+
+  private createPasswordLowercase(): BaseComponent<HTMLSpanElement> {
+    const passwordLowercase = createSpan(undefined, 'error-message');
+    passwordLowercase.setText('Password must contain at least one lowercase letter (a-z)');
+
+    return passwordLowercase;
+  }
+
+  private createPasswordUppercase(): BaseComponent<HTMLSpanElement> {
+    const passwordUppercase = createSpan(undefined, 'error-message');
+    passwordUppercase.setText('Password must contain at least one uppercase letter (A-Z)');
+
+    return passwordUppercase;
+  }
+
+  private createPasswordLength(): BaseComponent<HTMLSpanElement> {
+    const passwordLength = createSpan(undefined, 'error-message');
+    passwordLength.setText('Password must be at least 8 characters long');
+
+    return passwordLength;
+  }
+
+  private createPasswordTooltip(): BaseComponent<HTMLDivElement> {
+    const passwordTooltip = createDiv(undefined, 'tooltip');
+    passwordTooltip.getElement().hidden = true;
+
+    return passwordTooltip;
+  }
+
+  private createPasswordControl(): BaseComponent<HTMLDivElement> {
+    const passwordControl = createDiv(undefined, 'password-control');
+    passwordControl.getElement().insertAdjacentHTML('afterbegin', eyeClose);
+
+    return passwordControl;
+  }
+
+  private createPasswordInput(): BaseComponent<HTMLInputElement> {
+    const passwordInput = createInput(undefined, 'password');
+    const passwordInputElement = passwordInput.getElement();
+    passwordInputElement.placeholder = 'Enter your password';
+    passwordInputElement.type = InputType.PASSWORD;
+
+    return passwordInput;
+  }
+
+  private createSubmitButton(): BaseComponent<HTMLButtonElement> {
+    const submitButton = createButton(undefined, 'button');
+    submitButton.setText('Sign In');
+
+    return submitButton;
   }
 }
 
