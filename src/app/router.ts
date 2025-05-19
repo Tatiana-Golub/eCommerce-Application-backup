7fcc98252480ec53d189e1d16c5aaa34d2b33002
Eugeku <eugenekuzora@gmail.com>
2025-05-19 19:39:07 +0300

    fix: cleanup console logs

--- src/app/router.ts
+++ src/app/router.ts
@@ -15,13 +15,10 @@ class Router {
 
   public addRoute(path: string, handler: RouteHandler): void {
     this.routes.set(path, handler);
-    console.log('routes:');
-    console.log(this.routes);
   }
 
   public navigate(path: string): void {
     this.flag = true;
-    console.log(`navigating to path: ${path}`);
     globalThis.location.hash = path;
     if (this.flag) {
       this.handleRoute();
@@ -33,20 +30,14 @@ class Router {
     const currentHash = globalThis.location.hash
       ? globalThis.location.hash.replace('#', '')
       : initialRoute;
-    console.log(`initial hash: ${globalThis.location.hash}`);
     this.navigate(currentHash);
   }
 
   private handleRoute(): void {
     const path = globalThis.location.hash || '';
-
-    console.log('handleRoute:', path);
-
     const handler = this.routes.get(path) || this.routes.get('*');
     if (handler) {
       handler();
-    } else {
-      console.log(`Route not found: ${path}`);
     }
   }
 }
